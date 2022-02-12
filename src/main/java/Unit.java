4474574621c38b0d92e024463cf6005aa9cd3c3c
villi02 <villiarnar3@gmail.com>
2022-02-12 18:00:21 +0100

    Update Unit.java

--- src/main/java/Unit.java
+++ src/main/java/Unit.java
@@ -1,2 +1,37 @@
-package PACKAGE_NAME;public class Unit {
+public class Unit {
+    private String name;
+    private int health, attack, armor;
+
+    public Unit(String name, int health, int attack, int armor) {
+        this.name = name;
+        this.health = health;
+        this.attack = attack;
+        this.armor = armor;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public int getAttack() {
+        return attack;
+    }
+
+    public int getArmor() {
+        return armor;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Unit name: {0}, health: {1}, attack: {2}, armor: {3} ", getName(), getHealth(), getAttack(), getArmor());
+    }
+
 }
