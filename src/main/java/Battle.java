90a42ed2c133166087961c272a3f719662eca573
villi02 <villiarnar1@gmail.com>
2022-03-03 14:07:25 +0100

    small modifications to Battle class

--- src/main/java/Battle.java
+++ src/main/java/Battle.java
@@ -2,6 +2,12 @@ import Units.*;
 public class Battle {
 
     private Army armyOne, armyTwo;
+    enum State{
+        ARMY1_ATTACK,
+        ARMY2_ATTACK
+    }
+
+    private Battle.State myState  = State.ARMY1_ATTACK;
 
     public Battle(Army armyOne, Army armyTwo) {
         this.armyOne = armyOne;
@@ -9,7 +15,18 @@ public class Battle {
     }
 
     public void simulate(){
+        Army armyoneCopy = new Army(armyOne.getAllUnits(), "copy1");
+        Army armytwoCopy = new Army(armyTwo.getAllUnits(), "copy2");
+        while (armyOne.hasUnits() && armyTwo.hasUnits())
+        {
+            Unit armyoneTrooper = armyOne.getRandom();
+            Unit armytwoTrooper = armyTwo.getRandom();
 
+            switch(myState){
+                case ARMY1_ATTACK:
+                    // Gonna make a method to deal with attacks and defences
+            }
+        }
     }
 
     @Override
